0f1a1b87f54deb4cd92b72f00e02003680b5a3ff
Shreeharsha Shet <shreeharshashet@Shreeharshas-MacBook-Pro.local>
2018-06-16 20:36:46 +0530

    register with serviceregistry

--- serviceregistry/src/main/java/com/simply/tp/microfix/serviceregistry/ServiceregistryApplication.java
+++ serviceregistry/src/main/java/com/simply/tp/microfix/serviceregistry/ServiceregistryApplication.java
@@ -3,9 +3,11 @@ package com.simply.tp.microfix.serviceregistry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableEurekaServer
+@EnableConfigServer
 public class ServiceregistryApplication {
 
 	public static void main(String[] args) {
