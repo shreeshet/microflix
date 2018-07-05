7543bf04f190a11b527ccbd11ad84843957ff02a
Shreeharsha Shet <shreeharshashet@Shreeharshas-MacBook-Pro.local>
2018-07-05 23:01:07 +0530

    adding security config

--- inventoryservice/src/main/java/com/simply/tp/microflix/inventoryservice/config/SecurityConfig.java
+++ inventoryservice/src/main/java/com/simply/tp/microflix/inventoryservice/config/SecurityConfig.java
@@ -1,26 +1,14 @@
-package com.simply.tp.microflix.userservice.config;
+package com.simply.tp.microflix.inventoryservice.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.UserDetailsService;
 
 
 /**
  * Created by shreeharshashet on 7/3/18.
  */
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
-    @Autowired
-    private UserDetailsService userDetailsService;
-
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth
-                .userDetailsService(userDetailsService);
-    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
